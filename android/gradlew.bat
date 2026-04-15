ccd2f2e1a83032df90b8912a1d621614b45a180d
kayvirus <kayodemoland@gmail.com>
2026-04-15 15:18:26 +0100

    Delete android directory

--- android/gradlew.bat
+++
@@ -1,7 +0,0 @@
-@ECHO OFF
-SET DIR=%~dp0
-IF NOT EXIST "%DIR%gradle\wrapper\gradle-wrapper.jar" (
-  ECHO gradle-wrapper.jar missing. Run: gradle wrapper --gradle-version 8.10.2
-  EXIT /B 1
-)
-java -classpath "%DIR%gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
